59229d1dab4fdfc064bcfd5fdd2618848386d4c9
Spencer Butler <spencerb@honeycomb.net>
2021-11-20 10:09:19 -0600

    cleanup and update homedir script

---
+++ bin/homedir.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+# Spencer Butler <github@tcos.us>
+# fix up dot files, and shit
+
+DF_ARCHIVE="${HOME}/.dotfiles-archive"
+DF_DEST="${HOME}/.dotfiles"
+DF_REPO="https://github.com/spencerbutler/dotfiles.git"
+
+DOTFILES=".bashrc
+.bash_profile
+.bash_logout
+.vimrc
+.tmux.conf
+.gitconfig
+.screenrc
+.vim
+.git
+"
+
+[ -d $DF_ARCHIVE ] || mkdir $DF_ARCHIVE
+
+archive() {
+    local file=$1
+    local islink="$(readlink $file)"
+    local HERE="$PWD"
+    cd $HOME
+
+    if [ -n "$islink" ]; then
+      echo "==> Removing SymLink $file => $islink"
+      rm $file
+    elif [ -f "$file" -o -d "$file" ]; then
+      echo "==> Archiving $file => ${DF_ARCHIVE}/${file}"
+      mv $file ${DF_ARCHIVE}/
+    else
+      echo "==> $file doesn't exist"
+    fi
+    cd $HERE
+}
+
+install_file() {
+    local file="$1"
+    local HERE="$PWD"
+    cd $HOME
+
+    if [ -d "${DF_DEST}/${file}" -a "$file" = '.vim' ]; then
+      cp -av ${DF_DEST}/${file} $file/
+    fi
+
+    ln -s ${DF_DEST}/${file} .
+    cd $HERE
+}
+
+
+if [ ! -d "$DF_DEST" ]; then
+    cd $HOME
+    git clone $DF_REPO  $DF_DEST || { echo "Could not clone $DF_REPO"; exit 1; }
+else
+    cd $DF_DEST
+    git pull
+fi
+
+for dotfile in $DOTFILES; do
+    archive $dotfile
+    install_file $dotfile
+done
+
+source ${HOME}/.bashrc
+echo .
+echo ..
+echo ...
+echo ....
+echo "Your dotfile environment is ready to go!"
+
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Notes
+## Xshell
+- set terminal to xterm-256color
+- enable true color (tools->options->advanced)
+- enable "Treat ambiguously sized charachters as wide"
+## tmux
+- set default-terminal "xterm-256color"
